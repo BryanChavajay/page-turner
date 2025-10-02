a0f2ef1a27ba1df510844befbe697a61f990e780
Bryan Chavajay <bchavajaydev@gmail.com>
2025-10-01 22:43:22 -0600

    Backend: Add User and DTO models for user management

---
+++ backend/app/app/user/dtos.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class BaseUser(BaseModel):
+    email: EmailStr = Field(max_length=100)
+    username: str = Field(max_length=50, min_length=3)
+
+
+class PublicUser(BaseUser):
+    pass
+
+
+class UserCreate(BaseUser):
+    password: str = Field(max_length=50, min_length=6)
+
+
+class UserUpdate(BaseUser):
+    password: str | None = Field(default=None, max_length=50, min_length=6)
