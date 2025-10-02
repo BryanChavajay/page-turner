5297fc2d5bb7527e2ab41f9cfd1d5afc6784e3ee
Bryan Chavajay <bchavajaydev@gmail.com>
2025-10-01 22:43:57 -0600

    Backend: Implement SQLModelUserRepository for user management

---
+++ backend/app/app/user/infraestructure/postgres.py
@@ -0,0 +1,62 @@
+from sqlmodel import Session, select
+
+from app.core.sqlmodel_models import User as UserModel
+
+from app.app.user.port import UserRepository
+from app.app.user.domain import User
+from app.app.user.dtos import UserCreate, UserUpdate
+
+
+class SQLModelUserRepository(UserRepository):
+    def __init__(self, db: Session):
+        self.db = db
+
+    def find_by_id(self, user_id: int) -> User | None:
+        user = self.db.get(UserModel, user_id)
+        return User.model_validate(user) if user else None
+
+    def find_by_email(self, email: str) -> User | None:
+        statement = select(UserModel).where(UserModel.email == email)
+        user = self.db.exec(statement).first()
+        return User.model_validate(user) if user else None
+
+    def find_by_username(self, username: str) -> User | None:
+        statement = select(UserModel).where(UserModel.username == username)
+        user = self.db.exec(statement).first()
+        return User.model_validate(user) if user else None
+
+    def save(self, user: UserCreate) -> User:
+        user_db = UserModel(**user.model_dump())
+        hash_password = user.password  # Todo: Hash password
+        user_db.password = hash_password
+        self.db.add(user_db)
+        self.db.commit()
+        self.db.refresh(user_db)
+        return User.model_validate(user_db)
+
+    def update(self, user: UserUpdate) -> User:
+        statement = select(UserModel).where(UserModel.username == user.username)
+        user_db = self.db.exec(statement).one()
+        if not user.password:
+            hash_password: str = user_db.password
+            user_db.password = hash_password
+        user_db.email = user.email
+        user_db.username = user.username
+        user_db.session_version = user_db.session_version + 1
+        self.db.commit()
+        self.db.refresh(user_db)
+        return User.model_validate(user_db)
+
+    def delete(self, username: str) -> bool:
+        statement = select(UserModel).where(UserModel.username == username)
+        user_db = self.db.exec(statement).first()
+        self.db.delete(user_db)
+        self.db.commit()
+        self.db.refresh(user_db)
+
+        statement = select(UserModel).where(UserModel.username == username)
+        user_db = self.db.exec(statement).first()
+        if user_db is None:
+            return True
+
+        return False
---
+++ backend/app/app/user/port.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+from app.app.user.dtos import UserCreate, UserUpdate
+from app.app.user.domain import User
+
+
+class UserRepository(ABC):
+    @abstractmethod
+    def find_by_id(self, user_id: int) -> User | None:
+        pass
+
+    @abstractmethod
+    def find_by_email(self, email: str) -> User | None:
+        pass
+
+    @abstractmethod
+    def find_by_username(self, username: str) -> User | None:
+        pass
+
+    @abstractmethod
+    def save(self, user: UserCreate) -> User:
+        pass
+
+    @abstractmethod
+    def update(self, user: UserUpdate) -> User:
+        pass
+
+    @abstractmethod
+    def delete(self, username: str) -> bool:
+        pass
