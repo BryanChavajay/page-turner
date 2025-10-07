0b3c98969dcfc3740743d5e2fd8b6bdab97b5a78
Bryan Chavajay <bchavajaydev@gmail.com>
2025-10-06 23:02:58 -0600

    Backend: Add current user dependency to user routes for user retrieval

--- backend/app/api/v1/user.py
+++ backend/app/api/v1/user.py
@@ -6,7 +6,7 @@ from app.app.user.dtos import PublicUser, UserCreate, UserUpdate
 from app.app.user.application import UserService
 from app.app.user.infraestructure.postgres import SQLModelUserRepository
 
-from app.api.deps import SessionDep
+from app.api.deps import SessionDep, CurrentUserDep
 
 
 def get_user_service(db: SessionDep) -> UserService:
@@ -23,3 +23,9 @@ router = APIRouter(tags=["user"])
 def register_user(user: UserCreate, service: ServiceDep) -> Any:
     new_user = service.create_user(user)
     return new_user
+
+
+@router.get("/me", response_model=PublicUser)
+def get_my_user(current_user: CurrentUserDep, service: ServiceDep) -> Any:
+    user = service.get_user_by_username(current_user.username)
+    return user
