9bbaf68821b857631481d56b5be20ecc63cbf067
Bryan Chavajay <bchavajaydev@gmail.com>
2025-10-16 22:45:14 -0600

    frontend: initialize frontend with React, Vite, and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <section className="min-h-dvh bg-blue-900/75">
+        <article>
+          <h1 className="text-2xl">HOLA MUNDO</h1>
+        </article>
+        <p className="text-base">Configuraciones inciales</p>
+      </section>
+    </>
+  );
+}
+
+export default App;
