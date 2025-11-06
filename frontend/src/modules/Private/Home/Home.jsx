ce4401b4a8c6adbd0e0099d769102110a215715d
Bryan Chavajay <bchavajaydev@gmail.com>
2025-11-05 22:48:50 -0600

    Frontend: Add BookList component to Home page

--- frontend/src/modules/Private/Home/Home.jsx
+++ frontend/src/modules/Private/Home/Home.jsx
@@ -1,3 +1,5 @@
+import {BookList} from '@/components/BookList.jsx'
+
 export const Home = () => {
   return (
     <div className="relative z-10 min-h-screen">
@@ -18,7 +20,7 @@ export const Home = () => {
                 </p>
               </article>
             </section>
-            <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border border-accent/40 bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3 gap-2">
+            <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border border-border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3 gap-2">
               Añadir cita
             </button>
           </div>
@@ -39,6 +41,7 @@ export const Home = () => {
               </p>
             </article>
             <div>LISTA DE LIBROS</div>
+            <BookList />
           </section>
 
           {/* Finished Books Section */}
