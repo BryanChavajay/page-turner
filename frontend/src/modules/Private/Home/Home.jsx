6d8882dd149f8145299d73f201ee98b44285b7dc
Bryan Chavajay <bchavajaydev@gmail.com>
2025-11-12 23:00:10 -0600

    Frontend: Add AddQuote component and modal functionality to Home component

--- frontend/src/modules/Private/Home/Home.jsx
+++ frontend/src/modules/Private/Home/Home.jsx
@@ -1,16 +1,24 @@
-import {BookList} from '@/components/BookList.jsx'
-import {BookOpen} from '@/assets/BookOpen.jsx'
+import { BookList } from "@/components/BookList.jsx";
+import { BookOpen } from "@/assets/BookOpen.jsx";
+import { AddQuote } from "@/components/AddQuote.jsx";
 
 export const Home = () => {
+  const idQuoteDialog = "addQuoteDialog";
+
+  const openModal = () => {
+    document.getElementById(idQuoteDialog).showModal();
+  };
+
   return (
     <div className="relative z-10 min-h-screen">
       {/* HEADER */}
+      <AddQuote idDialog={idQuoteDialog}/>
       <header className="border-b border-border bg-card/50 backdrop-blur-sm sticky top-0 z-10">
         <div className="container mx-auto px-4 py-4">
           <div className="flex items-center justify-between">
             <section className="flex items-center gap-3">
               <article className="p-2 bg-primary/10 rounded-lg">
-                <BookOpen className="h-6 w-6 text-primary"/>
+                <BookOpen className="h-6 w-6 text-primary" />
               </article>
               <article>
                 <h1 className="text-2xl font-bold text-foreground">
@@ -21,7 +29,10 @@ export const Home = () => {
                 </p>
               </article>
             </section>
-            <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border border-border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3 gap-2">
+            <button
+              className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border border-border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3 gap-2"
+              onClick={openModal}
+            >
               Añadir cita
             </button>
           </div>
