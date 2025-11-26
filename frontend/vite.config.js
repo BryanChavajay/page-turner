e650c3b69a3f89ec80754309bb07aa1abd29801e
Bryan Chavajay <bchavajaydev@gmail.com>
2025-11-25 23:05:08 -0600

    Frontend: Add alias for private module in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@private": path.resolve(__dirname, "./src/modules/Private"),
     },
   },
 });
